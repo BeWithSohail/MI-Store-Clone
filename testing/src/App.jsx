6126382ce4729732faff7aaadbc2db9bb2b4ef36
BeWithSohail <sohailalam6027@gmail.com>
2024-04-20 21:12:05 +0530

    file initialized

---
+++ testing/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+// import Routerabc from './Routerabc';
+
+function App() {
+  console.log(process.env.REACT_APP_S3_BUCKET);
+
+  return (
+    <>
+        <h2> appfile</h2>
+    </>
+  )
+}
+
+export default App
---
+++ testing/src/Routerabc.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'dotenv/config';
+
+// const apiKey = process.env.REACT_APP_API_KEY;
+// const movieApiKey = process.env.REACT_APP_MOVIE_API_KEY;
+
+const Routerabc = () => {
+  // console.log("=============== abcdefghijklmno =============")
+  return (
+      <div>
+          <h2>
+        
+          </h2>
+
+          <h2>
+        
+          </h2>
+  
+      <h2> my name is md </h2>
+    </div>
+  )
+}
+
+export default Routerabc;
